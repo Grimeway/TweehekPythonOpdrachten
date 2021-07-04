19bccc5ec000dfa1c02b27d94ae2f20c01691506
Michel <m.kwantes@hotmail.com>
2021-07-04 14:16:20 +0200

    eerste setup

---
+++ Opdracht 1.py
@@ -0,0 +1,26 @@
+"""In deze opdrachten zul je de basis begrippen leren van Python programeren
+In de eerste opdrachten gaan we kijken naar output. Als je iets gedaan hebt wil je
+output hebben om te zien of het ding dat je gedaan heb, goed is.
+Output kan je krijgen door middel van de print() functie
+Hieronder zie je een voorbeeld
+"""
+
+print("Hallo world")
+
+print("michel")
+
+naam = "michel"
+
+print(naam)
+
+achternaam = input("geef je achternaam: \n")
+
+print(naam + str(50) + achternaam)
+
+
+
+
+#TODO: visual studio code of pycharm
+#TODO: Elke opdracht een eigen bestand
+#TODO: tot hoever willen we gaan, print statements ,variable, loops, som
+#TODO: variable uitprinten, optellen, aftrekken, keer, delen,
